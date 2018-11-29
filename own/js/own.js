4bcafa34d9e1d6b68e4aaca8ce05067c399766fd
nothasson <hassonl@foxmail.com>
2018-11-29 21:08:16 +0800

    学习js发送请求

--- own/js/own.js
+++ own/js/own.js
@@ -1,2 +1,41 @@
-var password = '123';
-var username = 'root';
+var loginClick = function()
+{
+    var username = document.getElementById('loginUser');
+    var password = hex_md5(document.getElementById('loginPassword'));
+    var PARAMS = {
+        username:username,
+        password,password
+    }
+    var httpRequest = new XMLHttpRequest();//第一步：创建需要的对象
+    httpRequest.open('POST', 'http://127.0.0.1:3000/checkPw', true); //第二步：打开连接
+    httpRequest.setRequestHeader("Content-type","application/x-www-form-urlencoded");//设置请求头 注：post方式必须设置请求头（在建立连接后设置请求头）
+    httpRequest.send('name=teswe&ee=ef');//发送请求 将情头体写在send中
+    /**
+     * 获取数据后的处理程序
+     */
+    httpRequest.onreadystatechange = function () {//请求后的回调接口，可将请求成功后要执行的程序写在其中
+        if (httpRequest.readyState == 4 && httpRequest.status == 200) {//验证请求是否发送成功
+            var json = httpRequest.responseText;//获取到服务端返回的数据
+            console.log(json);
+        }
+    };
+    //httpPost('http://127.0.0.1:3000/checkPw');
+}   
+function httpPost(URL, PARAMS) {
+    var temp = document.createElement("form");
+    temp.action = URL;
+    temp.method = "post";
+    temp.style.display = "none";
+
+    for (var x in PARAMS) {
+        var opt = document.createElement("textarea");
+        opt.name = x;
+        opt.value = PARAMS[x];
+        temp.appendChild(opt);
+    }
+
+    document.body.appendChild(temp);
+    temp.submit();
+
+    return temp;
+}
