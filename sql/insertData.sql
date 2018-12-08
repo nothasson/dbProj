45afe271465b528743ba3d20c0b8d4a4da82ba03
nothasson <hassonl@foxmail.com>
2018-12-09 01:14:59 +0800

    加了两个sql语句

--- sql/insertData.sql
+++ sql/insertData.sql
@@ -12,3 +12,9 @@ insert into tbteacour value('2002','101',300,4,1200);
 insert into tbteacour value('2101','103',400,3,1200);
 insert into tbteacour value('2101','104',100,2,200);
 insert into tbteacour value('2101','105',300,2,600);
+
+insert into tbuser value('root','ff9830c42660c1dd1942844f8069b74a','1');
+insert into tbuser value('green','4297f44b13955235245b2497399d7a93','0');
+
+insert into tbpower value('green','0','0','1');
+
--- sql/main.sql
+++ sql/main.sql
@@ -4,6 +4,8 @@ drop table if exists tbSchoolTea;
 drop table if exists tbTeaCour;
 drop table if exists tbCourse;
 drop table if exists tbTeacher;
+drop table if exists tbPower;
+drop table if exists tbUser;
 create table tbTeacher(
 	tNo varchar(16) primary key,
     tName varchar(16),
@@ -36,7 +38,9 @@ create table tbUser(
     
 create table tbPower(
 	username varchar(16),
-    power boolean,
+    powert boolean,
+    powerc boolean,
+    powertc boolean,
     foreign key(username) references tbUser(username))
     default	charset = utf8mb4;
 
